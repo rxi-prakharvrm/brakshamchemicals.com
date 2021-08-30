231367813c40d9468251ffdb254287053ecc63a1
Prakhar Verma <prakharverma1022@gmail.com>
2021-08-30 13:21:57 +0530

    final touch before hand over

--- js/index.js
+++ js/index.js
@@ -173,3 +173,38 @@ const sendBtn = document.querySelector('.send-mail-btn');
 sendBtn.addEventListener('click', () => {
     sendBtn.href = `mailto:contact@brakshamchemicals.com?subject=${msgSub.value}&body=${senderMob.value}  \n\n${msgBody.value}`
 })
+
+// Input focus effect
+
+msgSub.addEventListener('focus', () => {
+    msgSub.style.backgroundColor = "#fff"
+    msgSub.style.border = "2px solid #aaa"
+
+    senderMob.style.backgroundColor = "#f1f1f1"
+    senderMob.style.border = "none"
+
+    msgBody.style.backgroundColor = "#f1f1f1"
+    msgBody.style.border = "none"
+})
+
+senderMob.addEventListener('focus', () => {
+    msgSub.style.backgroundColor = "#f1f1f1"
+    msgSub.style.border = "none"
+
+    senderMob.style.backgroundColor = "#fff"
+    senderMob.style.border = "2px solid #aaa"
+
+    msgBody.style.backgroundColor = "#f1f1f1"
+    msgBody.style.border = "none"
+})
+
+msgBody.addEventListener('focus', () => {
+    msgSub.style.backgroundColor = "#f1f1f1"
+    msgSub.style.border = "none"
+
+    senderMob.style.backgroundColor = "#f1f1f1"
+    senderMob.style.border = "none"
+
+    msgBody.style.backgroundColor = "#fff"
+    msgBody.style.border = "2px solid #aaa"
+})
