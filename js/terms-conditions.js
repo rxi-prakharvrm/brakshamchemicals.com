ad5017f72b72d983ce965cda4839f5623c5b13b5
Prakhar Verma <prakharverma1022@gmail.com>
2021-09-10 09:07:46 +0530

    Adding an toggle animated hamberger menu bar with some design changes

--- js/terms-conditions.js
+++ js/terms-conditions.js
@@ -1,25 +1,16 @@
-let headerMenuBar = document.querySelector('.header-menu-bar-ctr');
 let headerMenu = document.querySelector('.header-menu');
-let headerMenuCloseBtn = document.querySelector('.header-menu-close-btn');
-let headerMenuBtn = document.querySelector('.header-menu-btn');
-let headerMenuInner = document.querySelector('.header-menu-inner');
-let covidUpdateCtr = document.querySelector('.covid-update-ctr');
-let covidUpdateCtrCloseBtn = document.querySelector('.covid-update-ctr-close-btn');
-let body = document.querySelector('body');
 
-headerMenuBar.addEventListener("click", () => {
-    headerMenu.style.marginTop = '0'
-    headerMenuInner.style.display = 'block'
-})
+// Header Menu Button Animation
+var line1 = document.querySelector('.line1');
+var line2 = document.querySelector('.line2');
+var line3 = document.querySelector('.line3');
 
-headerMenuCloseBtn.addEventListener("click", () => {
-    headerMenu.style.marginTop = '-30rem'
-    headerMenuInner.style.display = 'none'
-})
-
-headerMenuBtn.addEventListener("click", () => {
-    headerMenu.style.marginTop = '-30rem'    
-})
+function toggleMenu() {
+  line1.classList.toggle('line1-styled');
+  line2.classList.toggle('line2-styled');
+  line3.classList.toggle('line3-styled');
+  headerMenu.classList.toggle("toggle-menu");
+}
 
 // Copyright current year
 const date = new Date();
